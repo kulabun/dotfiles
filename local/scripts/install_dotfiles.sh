6b3cf80b990ee17e7689dc29405f03ad49de93fe
Konstantin Labun <konstantin.labun@gmail.com>
2018-12-02 01:18:18 -0800

    update install script description

--- local/scripts/install_dotfiles.sh
+++ local/scripts/install_dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# When you run command like cat file.sh | bash
-# input channel is overriden with /dev/null, so
-# 1) VIM fails with "Vim: Warning: Input is not from a terminal"
-# 2) yadm decrypt fails because it don't even ask you about password
+# Update script will update everything including vim plugins
+# the problem with vim is that it reads all the stdin input,
+# so program execution after it will not continue
+# when it runs over pipe
 DOTFILES_INSTALL_SCRIPT=/tmp/dotfiles.sh 
 curl --silent --output "$DOTFILES_INSTALL_SCRIPT" -L "https://raw.githubusercontent.com/kulabun/dotfiles/master/local/bin/update" 
 chmod +x "$DOTFILES_INSTALL_SCRIPT"
