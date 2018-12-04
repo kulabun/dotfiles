559ce7388bd041d0d48d229f70750d084641c9fb
Konstantin Labun <konstantin.labun@gmail.com>
2018-12-03 18:58:34 -0800

    update configuration

--- .config/fish/my-functions/ef.fish
+++ .config/fish/my-functions/ef.fish
@@ -1,7 +1,29 @@
 function ef
-    set -l func_file_path "$FISH_CUSTOM_FUNCTIONS_PATH"/"$argv[1].fish"
+    set -l ENCRYPT_FILE "$HOME"/.yadm/encrypt
+
+    # Validate function name provided
+    test (count $argv) -eq 0; and echo "ERROR: function name required"; and return 1
+
+    set -l name $argv[1]
+    set -l class ""
+    set -l suffix ""
+
+    # Check if we have class for file (YADM)
+    test (count $argv) -eq 2; and set -l class "$argv[2]"; and set -l suffix "##$class"
+
+    set -l func_file_path "$FISH_CUSTOM_FUNCTIONS_PATH"/"$name.fish$suffix"
+
     eval $EDITOR "$func_file_path" 
+    not test -e "$func_file_path"; and return 1;
+
     source "$func_file_path"
+    yadm add "$func_file_path" 1>&2 2> /dev/null
+
+    if [ "$class" = "work" ]
+        if ! cat "$ENCRYPT_FILE" | grep "$func_file_path" > /dev/null
+            echo "$func_file_path" | sed "s|$HOME/||" >> "$HOME"/.yadm/encrypt
+        end
+    end
 end
 
 complete -e -c ef
