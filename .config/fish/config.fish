752f35e15ccf86060bd02834e9255d546d327129
Konstantin Labun <konstantin.labun@gmail.com>
2019-08-24 23:09:17 -0700

    update configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,7 @@ set SPACEFISH_VI_MODE_REPLACE_ONE "^"
 set SPACEFISH_VI_MODE_COLOR "0BB087"
 set SPACEFISH_PROMPT_ORDER user host dir vi_mode
 set SPACEFISH_RPROMPT_ORDER git jobs aws exec_time exit_code
+set ASK_DEFAULT_DEVICE_LOCALE "en-US"
 #set -x AWS_PROFILE default # Override in session to use another profile
 
 #################### PLUGINS ####################
